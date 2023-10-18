bb9d47cc3e2bca0929b9c43ad343aed57c62176c
Muhammad Hammad <75518606+Hammad2910@users.noreply.github.com>
2023-10-18 16:18:44 +0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-account_sid="ACd580f3e1f823c368452baf5f51c86e66"
-auth_token="02801d4e1c350ca4a9c30a17a1f50e9b"
-twilio_number="+12296336301"
-my_number="+923007435532"
+account_sid=""
+auth_token=""
+twilio_number=""
+my_number=""
